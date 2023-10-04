597e1b764a452b3580a8e0557644f7819d40b66e
Alss019 <plaacorentin@gmail.com>
2023-10-04 14:44:02 +0200

    second commit

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,26 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte monByte = 1;
+		short monShort = 2;
+		int monInt = 3;
+		long monLong = 4;
+		float monFloat = 5;
+		double monDouble = 6;
+		char monChar = 7;
+		boolean monBoolean= true;
+		String monString = "123";
+		
+		System.out.println(monByte);
+		System.out.println(monString);
+		
+		String randomString = "Voici le resultat d'un calcul: \n1+5=6";
+		System.out.println(randomString);
+		
+	}
+
+}
