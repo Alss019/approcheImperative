571689e166a03f8de0eaa65accf1a6dbf0e06a9c
Alss019 <plaacorentin@gmail.com>
2023-10-05 18:39:33 +0200

    modif InteractifStockageNombre

--- src/fr/algorithmie/InteractifStockageNombre.java
+++ src/fr/algorithmie/InteractifStockageNombre.java
@@ -14,38 +14,26 @@ public class InteractifStockageNombre {
 		int option = 0;
 		System.out.println("Tapez 1 pour ajouter un nombre Tapez 2 pour voir votre tableau");
 		option = scanner.nextInt();
-		int i = 0;
+		int data = 0;
+		int size = 0;
 		
 		while(open == false) {	
 			if (option == 1) {
-				System.out.println("option 1");
 				System.out.println("Ajouter un chiffre");
-				if(i == 0) {
-					option = scanner.nextInt();
-					tab.add(option);
-					i++;
-					System.out.println(option + " ajouter au tableau");
-					
-				}else {
-					option = scanner.nextInt();
-					tab.add(option);
-					i++;
-					System.out.println("ajouter");
+				data = scanner.nextInt();
+				tab.add(data);
+				System.out.println("Ajouter au tableau");
 				}	
-				System.out.println("Tapez 1 pour ajouter un nombre Tapez 2 pour voir votre tableau");
-				option = scanner.nextInt();
-			}
-			if(option == 2){
+			else if(option == 2){
 				System.out.println(tab);
-				System.out.println("option 2");
-				System.out.println("Tapez 1 pour ajouter un nombre Tapez 2 pour voir votre tableau");
-				option = scanner.nextInt();
 			}
+			else {
+				System.out.println("Erreur");
+			}
+			System.out.println("Tapez 1 pour ajouter un nombre Tapez 2 pour voir votre tableau");
+			option = scanner.nextInt();
 		}
-		
-		
-		
-		
 	}
+		
 
 }
